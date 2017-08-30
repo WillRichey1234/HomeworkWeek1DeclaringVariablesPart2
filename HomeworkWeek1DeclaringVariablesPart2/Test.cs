07df607a17b39c5d8a887fcdcb7a8f449d15d292
WillRichey1234 <gsampson@vms.edu>
2017-08-30 15:31:00 -0600

    Declaring Variables part 2

---
+++ HomeworkWeek1DeclaringVariablesPart2/Test.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+namespace HomeworkWeek1DeclaringVariablesPart2
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            /*The capitalization allows me to
+             * make another variable with
+             * the exact same words but different
+             * capitalization and have them both mean two 
+             * different things. Try not to do this thouhg
+             */
+         /* The var creates a variable and does the
+          * same thing as a int or a string. It must be set
+          * equal to something
+          */
+           // string myFirstName = "Garrett";
+            //Console.WriteLine(myFirstName);
+            //Console.ReadLine();
+            /* This will write on the screen whatever
+		  * I plug in for the variable myFirstName
+		  */
+
+            int x = 7;
+            string y = "Garrett";
+            string myFirstTry = x.ToString()  + y;
+            Console.WriteLine(myFirstTry);
+            Console.ReadLine();
+
+		}
+    }
+}
